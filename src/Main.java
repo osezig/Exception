a4ae63b87ab29b0116e1179e2afb824d159887e3
sergey_iagfarov <dedaz27@mail.ru>
2022-12-16 21:41:38 +0500

    Домашняя работа

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean success = Data.validate("test", "test", "test");
+        if (success){
+            System.out.println("Данные валидны");
+        }else {
+            System.out.println("Данные невалидны");
+        }
     }
 }
